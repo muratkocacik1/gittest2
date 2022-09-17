6ae637a3a7b9c51635cc3426b4abf2e8e5772ade
Murat Kocacık <113637437+muratkocacik1@users.noreply.github.com>
2022-09-17 18:02:00 +0300

    Add files via upload

---
+++ GitTestProject/GitTestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Murat KOCACIK on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("git")
+        print("gitgit")
+
+    }
+
+
+}
+
